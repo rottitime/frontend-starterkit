9fcdf8783cd841a4b302e3a64552d8ef61a62929
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-01-18 12:41:11 +0000

    React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,32 @@
-import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { AppProps } from 'next/app'
+import { NextPage } from 'next'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface MyAppProps extends AppProps {
+  Component: NextPage & {
+    getLayout?: (page: ReactNode) => ReactNode
+  }
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 5 * 1000 } }
+})
+
+export default function MyApp({ Component, pageProps }: MyAppProps) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        {getLayout(<Component {...pageProps} />)}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  )
 }
