5718f5e604e0ca1189bcf17b4a6658192431b145
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-03-16 15:46:19 +0000

    jsconfig types

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 })
 
 // Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
 const customJestConfig = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
