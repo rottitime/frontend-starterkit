2edf832639c2995be49f8aa78db6f11f29d855a3
Jas <webmaster@jaspaul.com>
2024-02-04 18:03:17 +0000

    jest updates

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,6 @@ const createJestConfig = nextJest({
   dir: './'
 })
 
-// Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
@@ -21,8 +20,6 @@ const config: Config = {
     '<rootDir>/dist'
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom'
-import '@testing-library/jest-dom/extend-expect'
 import { enableFetchMocks } from 'jest-fetch-mock'
 
 // global.TextEncoder = require('util').TextEncoder
